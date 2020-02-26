b0e51374315a702bd1451fcff7b207a0fc8cef6e
Evgeniy Panin <mr.evgeniy.panin@yandex.ru>
2020-02-26 17:22:26 +0300

    add style

--- src/Controller/Main/HomeController.php
+++ src/Controller/Main/HomeController.php
@@ -3,15 +3,15 @@
 
 namespace App\Controller\Main;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-class HomeController extends AbstractController
+class HomeController extends BaseController
 {
     /**
      * @Route("/", name="home")
      */
     public function index()
     {
-
+        $forRender = parent::renderDefault();
+        return $this->render('main/index.html.twig', $forRender);
     }
 }
