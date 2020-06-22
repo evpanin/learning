f15b9585731b6395bf511b62637420b37f5e57c6
Evgeniy Panin <mr.evgeniy.panin@yandex.ru>
2020-06-22 20:10:46 +0300

    #10 videos

--- src/Controller/Admin/AdminPostController.php
+++ src/Controller/Admin/AdminPostController.php
@@ -7,6 +7,7 @@ namespace App\Controller\Admin;
 use App\Entity\Category;
 use App\Entity\Post;
 use App\Form\PostType;
+use App\Service\FileManagerServiceInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -33,15 +34,21 @@ class AdminPostController extends AdminBaseController
     /**
      * @Route("/admin/post/create", name="admin_post_create")
      * @param Request $request
+     * @param FileManagerServiceInterface $fileManagerService
      * @return RedirectResponse|Response
      */
-    public function create(Request $request)
+    public function create(Request $request, FileManagerServiceInterface $fileManagerService)
     {
         $em = $this->getDoctrine()->getManager();
         $post = new Post();
         $form = $this->createForm(PostType::class, $post);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()){
+            $image = $form->get('image')->getData();
+            if ($image){
+                $fileName = $fileManagerService->imagePostUpload($image);
+                $post->setImage($fileName);
+            }
             $post->setCreateAtValue();
             $post->setUpdateAtValue();
             $post->setIsPublished();
@@ -61,9 +68,10 @@ class AdminPostController extends AdminBaseController
      * @Route("/admin/post/update/{id}", name="admin_post_update")
      * @param int $id
      * @param Request $request
+     * @param FileManagerServiceInterface $fileManagerService
      * @return RedirectResponse|Response
      */
-    public function update(int $id, Request $request)
+    public function update(int $id, Request $request, FileManagerServiceInterface $fileManagerService)
     {
         $em = $this->getDoctrine()->getManager();
         $post = $this->getDoctrine()->getRepository(Post::class)
@@ -72,10 +80,23 @@ class AdminPostController extends AdminBaseController
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()){
             if ($form->get('save')->isClicked()){
+                $image = $form->get('image')->getData();
+                $imageOld = $post->getImage();
+                if ($image){
+                    if ($imageOld){
+                        $fileManagerService->removePostImage($imageOld);
+                    }
+                    $fileName = $fileManagerService->imagePostUpload($image);
+                    $post->setImage($fileName);
+                }
                 $post->setUpdateAtValue();
                 $this->addFlash('success', 'Пост обновлен');
             }
             if ($form->get('delete')->isClicked()){
+                $image = $post->getImage();
+                if ($image){
+                    $fileManagerService->removePostImage($image);
+                }
                 $em->remove($post);
                 $this->addFlash('success', 'Пост удален');
             }
